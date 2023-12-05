6a444f1a23ca1fa1a6f57db8d6cc0a4c8bf31352
muraca <mmuraca247@gmail.com>
2023-12-05 11:33:40 +0100

    day 03 problem 2
    Signed-off-by: muraca <mmuraca247@gmail.com>

--- src/day03.rs
+++ src/day03.rs
@@ -97,3 +97,129 @@ fn day03_problem1_solution() {
     let solution = day03_problem1(crate::lines_from_file("inputs/03.txt"));
     println!("Solution for day 03 problem 1: {}", solution);
 }
+
+/// The engineer finds the missing part and installs it in the engine!
+/// As the engine springs to life, you jump in the closest gondola, finally ready to ascend to the water source.
+///
+/// You don't seem to be going very fast, though. Maybe something is still wrong?
+/// Fortunately, the gondola has a phone labeled "help", so you pick it up and the engineer answers.
+///
+/// Before you can explain the situation, she suggests that you look out the window.
+/// There stands the engineer, holding a phone in one hand and waving with the other.
+/// You're going so slowly that you haven't even left the station. You exit the gondola.
+///
+/// The missing part wasn't the only issue - one of the gears in the engine is wrong.
+/// A gear is any * symbol that is adjacent to exactly two part numbers.
+/// Its gear ratio is the result of multiplying those two numbers together.
+///
+/// This time, you need to find the gear ratio of every gear and add them all up
+/// so that the engineer can figure out which gear needs to be replaced.
+///
+/// Consider the same engine schematic again:
+/// ```
+/// 467..114..
+/// ...*......
+/// ..35..633.
+/// ......#...
+/// 617*......
+/// .....+.58.
+/// ..592.....
+/// ......755.
+/// ...$.*....
+/// .664.598..
+/// ```
+/// In this schematic, there are two gears.
+/// The first is in the top left; it has part numbers 467 and 35, so its gear ratio is 16345.
+/// The second gear is in the lower right; its gear ratio is 451490.
+/// (The * adjacent to 617 is not a gear because it is only adjacent to one part number.)
+/// Adding up all of the gear ratios produces 467835.
+///
+/// What is the sum of all of the gear ratios in your engine schematic?
+pub fn day03_problem2(input: Vec<String>) -> u32 {
+    input
+        .iter()
+        .enumerate()
+        .map(|(i, line)| {
+            line.chars()
+                .enumerate()
+                // filter_map gives random errors with lifetimes
+                .map(|(j, char)| match char {
+                    '*' => {
+                        [
+                            get_num(&line.chars().collect(), j),
+                            i.checked_sub(1)
+                                .and_then(|idx| {
+                                    input.get(idx).map(|v| get_num(&v.chars().collect(), j))
+                                })
+                                .unwrap_or_default(),
+                            input
+                                .get(i + 1)
+                                .map(|btm| get_num(&btm.chars().collect(), j))
+                                .unwrap_or_default(),
+                        ]
+                        .into_iter()
+                        .flatten()
+                        .enumerate()
+                        .fold((0, 0), |acc, (idx, num)| match idx {
+                            0 => (num, 0),
+                            1 => (0, acc.0 * num),
+                            _ => (0, 0),
+                        })
+                        .1
+                    }
+
+                    _ => 0,
+                })
+                .sum::<u32>()
+        })
+        .sum()
+}
+
+fn get_num(ch: &Vec<char>, at: usize) -> Vec<u32> {
+    if !matches!(ch.get(at), Some(c) if c.is_ascii_digit()) {
+        let mut res = vec![];
+        if at > 0 && matches!(ch.get(at - 1), Some(c) if c.is_ascii_digit()) {
+            res.extend(get_num(ch, at - 1));
+        }
+        if at < ch.len() - 1 && matches!(ch.get(at + 1), Some(c) if c.is_ascii_digit()) {
+            res.extend(get_num(ch, at + 1));
+        }
+        return res;
+    }
+
+    let l = (0..at)
+        .rev()
+        .find(|&i| !matches!(ch.get(i), Some(c) if c.is_ascii_digit()))
+        .map(|i| i + 1);
+    let mut res = 0;
+    for i in l.unwrap_or_default().. {
+        match ch.get(i) {
+            Some(c) if c.is_ascii_digit() => res = res * 10 + c.to_digit(10).unwrap(),
+            _ => break,
+        }
+    }
+    vec![res]
+}
+
+#[test]
+fn day03_problem2_test() {
+    let input = vec![
+        "467..114..".to_string(),
+        "...*......".to_string(),
+        "..35..633.".to_string(),
+        "......#...".to_string(),
+        "617*......".to_string(),
+        ".....+.58.".to_string(),
+        "..592.....".to_string(),
+        "......755.".to_string(),
+        "...$.*....".to_string(),
+        ".664.598..".to_string(),
+    ];
+    assert_eq!(day03_problem2(input), 467835);
+}
+
+#[test]
+fn day03_problem2_solution() {
+    let solution = day03_problem2(crate::lines_from_file("inputs/03.txt"));
+    println!("Solution for day 03 problem 2: {}", solution);
+}
