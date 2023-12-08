af271e13407abed618a6666d1cf74aa8a96a1e36
muraca <mmuraca247@gmail.com>
2023-12-08 18:34:42 +0100

    day 08 problem 2
    Signed-off-by: muraca <mmuraca247@gmail.com>

--- src/day08.rs
+++ src/day08.rs
@@ -42,19 +42,7 @@
 /// ```
 /// Starting at AAA, follow the left/right instructions. How many steps are required to reach ZZZ?
 pub fn problem1(input: Vec<String>) -> usize {
-    let instructions: Vec<bool> = input[0]
-        .chars()
-        .map(|c| match c {
-            'L' => false,
-            'R' => true,
-            c => panic!("Invalid instruction: '{c}'"),
-        })
-        .collect();
-
-    let graph: std::collections::HashMap<&str, (&str, &str)> = input[2..]
-        .iter()
-        .map(|line| (&line[0..3], (&line[7..10], &line[12..15])))
-        .collect();
+    let (instructions, graph) = parse_input(input);
 
     let mut current = "AAA";
     let mut steps = 0;
@@ -68,6 +56,108 @@ pub fn problem1(input: Vec<String>) -> usize {
     }
 
     steps
+}
+
+/// The sandstorm is upon you and you aren't any closer to escaping the wasteland.
+/// You had the camel follow the instructions, but you've barely left your starting position.
+/// It's going to take significantly more steps to escape!
+///
+/// What if the map isn't for people - what if the map is for ghosts?
+/// Are ghosts even bound by the laws of spacetime? Only one way to find out.
+///
+/// After examining the maps a bit longer, your attention is drawn to a curious fact:
+/// the number of nodes with names ending in A is equal to the number ending in Z!
+/// If you were a ghost, you'd probably just start at every node that ends with A and follow all of the paths
+/// at the same time until they all simultaneously end up at nodes that end with Z.
+///
+/// For example:
+/// ```
+/// LR
+///
+/// 11A = (11B, XXX)
+/// 11B = (XXX, 11Z)
+/// 11Z = (11B, XXX)
+/// 22A = (22B, XXX)
+/// 22B = (22C, 22C)
+/// 22C = (22Z, 22Z)
+/// 22Z = (22B, 22B)
+/// XXX = (XXX, XXX)
+/// ```
+/// Here, there are two starting nodes, 11A and 22A (because they both end with A).
+/// As you follow each left/right instruction, use that instruction to simultaneously navigate away from both
+/// nodes you're currently on. Repeat this process until all of the nodes you're currently on end with Z.
+/// (If only some of the nodes you're on end with Z, they act like any other node and you continue as normal.)
+/// In this example, you would proceed as follows:
+/// - Step 0: You are at 11A and 22A.
+/// - Step 1: You choose all of the left paths, leading you to 11B and 22B.
+/// - Step 2: You choose all of the right paths, leading you to 11Z and 22C.
+/// - Step 3: You choose all of the left paths, leading you to 11B and 22Z.
+/// - Step 4: You choose all of the right paths, leading you to 11Z and 22B.
+/// - Step 5: You choose all of the left paths, leading you to 11B and 22C.
+/// - Step 6: You choose all of the right paths, leading you to 11Z and 22Z.
+///
+/// So, in this example, you end up entirely on nodes that end in Z after 6 steps.
+///
+/// Simultaneously start on every node that ends with A.
+/// How many steps does it take before you're only on nodes that end with Z?
+pub fn problem2(input: Vec<String>) -> usize {
+    let (instructions, graph) = parse_input(input);
+
+    graph
+        .iter()
+        .filter_map(|(name, _)| {
+            if name.ends_with('A') {
+                let mut current = name;
+                let mut steps = 0;
+                while !current.ends_with('Z') {
+                    let (left, right) = graph.get(current).unwrap();
+                    current = match instructions[steps % instructions.len()] {
+                        false => left,
+                        true => right,
+                    };
+                    steps += 1;
+                }
+                Some(steps)
+            } else {
+                None
+            }
+        })
+        .fold(1, lcm)
+}
+
+// lowest common multiple
+fn lcm(a: usize, b: usize) -> usize {
+    a * b / gcd(a, b)
+}
+
+// greatest common divisor
+fn gcd(a: usize, b: usize) -> usize {
+    match b == 0 {
+        true => a,
+        false => gcd(b, a % b),
+    }
+}
+
+fn parse_input(
+    input: Vec<String>,
+) -> (
+    Vec<bool>,
+    std::collections::HashMap<String, (String, String)>,
+) {
+    (
+        input[0]
+            .chars()
+            .map(|c| match c {
+                'L' => false,
+                'R' => true,
+                c => panic!("Invalid instruction: '{c}'"),
+            })
+            .collect(),
+        input[2..]
+            .iter()
+            .map(|line| (line[0..3].into(), (line[7..10].into(), line[12..15].into())))
+            .collect(),
+    )
 }
 
 #[cfg(test)]
@@ -87,6 +177,14 @@ mod test {
             6
         );
     }
+
+    #[test]
+    fn problem2() {
+        assert_eq!(
+            super::problem2(crate::lines_from_file("inputs/08-example3.txt")),
+            6
+        );
+    }
 }
 
 #[cfg(test)]
@@ -95,5 +193,11 @@ mod solution {
     fn problem1() {
         let solution = super::problem1(crate::lines_from_file("inputs/08.txt"));
         println!("Solution for day 08 problem 1: {}", solution);
+    }
+
+    #[test]
+    fn problem2() {
+        let solution = super::problem2(crate::lines_from_file("inputs/08.txt"));
+        println!("Solution for day 08 problem 2: {}", solution);
     }
 }
